6d76c8b52391d33f9c3a6c66e99003c80dd1ef04
Taehyeong Kim <th.imk3@gmail.com>
2023-04-22 19:56:01 +0900

    init base crkbd setting

---
+++ keyboards/crkbd/rev1/keymaps/taehyeong/rules.mk
@@ -0,0 +1,10 @@
+# Copyright 2022 LucW (@luc-languagetools)
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+# RGB_MATRIX_ENABLE = no
+OLED_ENABLE     = yes
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+SPLIT_KEYBOARD = yes
