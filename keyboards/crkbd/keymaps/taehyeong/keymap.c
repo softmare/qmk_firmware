7fea3be780bd00bbd39dabfbe71f08350fc219c4
Taehyeong Kim <th.imk3@gmail.com>
2023-09-02 18:50:01 +0900

    change gamemode location and add KC_GRV

--- keyboards/crkbd/keymaps/taehyeong/keymap.c
+++ keyboards/crkbd/keymaps/taehyeong/keymap.c
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
   [_LOWER] = LAYOUT_split_3x6_3(
-        KC_NO, KC_CIRC, KC_AT, KC_NO, KC_NO, KC_NO,                        KC_NO, KC_NO, KC_PERC, KC_DLR, KC_SLSH, KC_NO,
+        KC_NO, KC_CIRC, KC_AT, KC_GRV, KC_NO, KC_NO,                        KC_NO, KC_NO, KC_PERC, KC_DLR, KC_SLSH, KC_NO,
         KC_NO, KC_EQL, KC_COLN, KC_NUBS, KC_AMPR, KC_NO,                   KC_NO, KC_PIPE, KC_PLUS, KC_MINS, KC_ASTR, KC_ENT,
         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
                             KC_NO, KC_TRNS, KC_TRNS,                      KC_TRNS, KC_TRNS, KC_NO
@@ -120,20 +120,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_MPLY, KC_VOLU, KC_MUTE, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_MPRV, KC_VOLD, KC_MNXT, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                                   KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
+                                   KC_TRNS, GAME   , KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [_GAME] = LAYOUT_split_3x6_3(
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_RALT, KC_DEL,
         KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_P, KC_ENT,
         KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_CAPS,
-        KC_LALT, GAME_LT, KC_SPC, KC_TRNS, KC_TRNS, GAME
+        KC_LALT, GAME_LT, KC_SPC, KC_TRNS, KC_TRNS, KC_TRNS
   ),
   [_GAME_LT] = LAYOUT_split_3x6_3(
-        KC_TRNS, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        GAME   , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_ESC , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, GAME_LT, KC_TRNS, KC_TRNS, KC_TRNS, GAME
+        KC_TRNS, GAME_LT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 };
 
